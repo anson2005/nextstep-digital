efc869c5804ae2a4d1f2373cf3885b579c141712
Anson Joseph <ansonjosephhh@gmail.com>
2026-08-09 01:29:14 +0530

    Fix static deployment and article routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
- site: "http://localhost:4321",
+  site: 'https://nextstepdigital.in',
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
 });
