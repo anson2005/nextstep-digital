9ff5420483fb9b14849e6bfe392e831adef41139
Anson Joseph <ansonjosephhh@gmail.com>
2026-08-11 21:19:07 +0530

    Set www as canonical site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://nextstepdigital.in',
+  site: 'https://www.nextstepdigital.in',
   integrations: [tailwind(), sitemap()],
   output: 'static',
 });
